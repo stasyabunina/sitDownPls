f22b8a5858d5092963953970cc0032ed4444bf38
stasyabunina <118092308+stasyabunina@users.noreply.github.com>
2024-08-12 00:55:59 +0300

    Delete src/js directory

--- src/js/components/tooltip.js
+++
@@ -1,7 +0,0 @@
-if (document.querySelector(".feedback")) {
-  tippy('.feedback__tooltip', {
-    content: 'Реплицированные с зарубежных источников, исследования формируют глобальную сеть.',
-    theme: 'gray',
-    maxWidth: 190,
-  });
-}
